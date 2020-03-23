29c4151f84adc8929ee880ebe8b00b2a8e6c0c30
ksimaster <ksimasterlord@gmail.com>
2020-03-23 21:21:31 +0300

    Создан общий аниматор, скрипт не настроен, добавлены доп анимации
    Создан общий аниматор, скрипт не настроен, добавлены доп анимации

---
+++ Solman Project/Assets/script/AnimatorScript.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class SimplePlayer : MonoBehaviour
+{
+
+    Animator animator;
+    bool otgim = false;
+
+    
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+       // bool fire = Input.GetButtonDown("Fire1");
+
+        
+      //  animator.SetBool("Fire", fire);
+    }
+
+    void Otgimanie_Bttn()
+    {
+        otgim = true;
+        animator.SetBool("otgim", otgim);
+    }
+
+    void Otgim_Prigok_Bttn()
+    {
+
+    }
+
+    void Planka_Bttn()
+    {
+
+    }
+
+    void Prigok_Bttn()
+    {
+
+    }
+
+    void Velosiped_Bttn()
+    {
+
+    }
+
+    void Voen_prigok_Bttn()
+    {
+
+    }
+
+
+}
